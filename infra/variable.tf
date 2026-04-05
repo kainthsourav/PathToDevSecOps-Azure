1de219d5c0f35db876364865b489856f60d22b73
Sourav Kainth <souravkainth@outlook.com>
2026-04-05 19:33:12 +0530

    fixed variable.tf

--- infra/variable.tf
+++ infra/variable.tf
@@ -1,41 +1,38 @@
-variable "resouce_group_name" {
-  description = "Resource group name for demo api infra"
+variable "resource_group_name" {
+  description = "Resource group for DemoApi infrastructure"
   type        = string
 }
 
 variable "location" {
-  description = "Location for demo api infra"
+  description = "Azure region"
   type        = string
+  default     = "eastasia"
 }
 
 variable "acr_name" {
-  description = "ACR name for demo api infra"
+  description = "Azure Container Registry name"
   type        = string
 }
 
-variable "aks_name" {
-  description = "AKS name for demo api infra"
+variable "aks_cluster_name" {
+  description = "AKS cluster name"
   type        = string
 }
 
 variable "aks_node_count" {
-  description = "AKS node count for demo api infra"
+  description = "Number of worker nodes"
   type        = number
   default     = 1
 }
 
 variable "aks_node_size" {
-  description = "AKS node size for demo api infra"
+  description = "VM size for AKS nodes"
   type        = string
   default     = "Standard_D4d_v4"
 }
+
 variable "environment" {
-  description = "Environment for demo api infra"
+  description = "Environment tag"
   type        = string
   default     = "dev"
-}
-variable "project" {
-  description = "Project name for demo api infra"
-  type        = string
-  default     = "demo-api"
 }
