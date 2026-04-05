d764619b43d38e9d32fd394eaf9790aa23930196
Sourav Kainth <souravkainth@outlook.com>
2026-04-05 18:32:01 +0530

    Fix : Terraform Plan

--- infra/backend.tf
+++ infra/backend.tf
@@ -4,14 +4,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "~> 3.0"
     }
   }
+
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstatesouravdemo"
-    container_name       = "tfstate"
-    key                  = "demoapi.terraform.tfstate"
+    use_azuread_auth = true  # uses SP identity — no storage keys needed
   }
 }
 
